76cfc97b3cf88f8f2d7c4825cc6af1452c366218
Oat Asdon <o.asdon@gmail.com>
2016-04-04 10:41:28 -0700

    setup user model

--- models/user.js
+++ models/user.js
@@ -2,9 +2,20 @@ var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
 
 var userSchema = new mongoose.Schema({
-  name:   String,
-  handle: String
+  first_name:     { type: String, required: true },
+  last_name:      { type: String, required: true },
+  display_name:   { type: String, required: true, unique: true },
+  email:          { type: String, required: true, unique: true }
 });
+
+userSchema.plugin(require('mongoose-bcrypt'));
+
+userSchema.options.toJSON = {
+  transform: function(document, returnedObject, options) {
+    delete returnedObject.password;
+    return returnedObject;
+  }
+};
 
 var User = mongoose.model('User', userSchema);
 
