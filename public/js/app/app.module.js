5b139ee430a6c701ab94d27632fcb2ee5ff90df8
Oat Asdon <o.asdon@gmail.com>
2016-04-05 09:10:25 -0700

    inject ngMaterial

--- public/js/app/app.module.js
+++ public/js/app/app.module.js
@@ -2,6 +2,17 @@
   'use strict';
 
   angular
-    .module('app', ['ui.router']);
-
+    .module('app', ['ui.router', 'ngMaterial'])
+    .config(function($mdThemingProvider) {
+      $mdThemingProvider.theme('default')
+        .primaryPalette('red', {
+          'default': '500',
+          'hue-1':   '100',
+          'hue-2':   '600',
+          'hue-3':   'A100'
+        })
+        .accentPalette('grey', {
+          'default': '200'
+        });
+      });
 })();
