8147745fe8e665b9872546a740091a259db6bbae
Oat Asdon <o.asdon@gmail.com>
2016-04-05 09:11:02 -0700

    add navbar controller

---
+++ public/js/app/controllers/navbar.controller.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('NavbarController', NavbarController)
+
+    NavbarController.$inject = ['$log'];
+
+    function NavbarController($log) {
+      var vm = this;
+
+      // Bindings
+
+
+      // Methods
+
+
+
+
+
+
+    };
+
+})();
