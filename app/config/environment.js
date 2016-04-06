8200c4f229605a4e8b3a5a7d4290bd8c7f9bf876
Oat Asdon <o.asdon@gmail.com>
2016-04-06 16:14:26 -0700

    clean up file

--- app/config/environment.js
+++ app/config/environment.js
@@ -1,9 +1,9 @@
 var _ = require('lodash');
 
 var localEnvVars = {
-  TITLE:      'panda_working/',
-  SAFE_TITLE: 'panda_working/',
-  COOKIE_SECRET: 'securetocookiedesu',
+  TITLE:          'panda_working/',
+  SAFE_TITLE:     'panda_working/',
+  COOKIE_SECRET:  'securetocookiedesu',
   SESSION_SECRET: 'securetoseshonudesu',
   TOKEN_SECRET:   'superusecuretotokenodesu'
 };
