0c833a17adfbb2160587d7e8be0fd14e344254b1
Oat Asdon <o.asdon@gmail.com>
2016-04-06 15:31:46 -0700

    add environment secrets

--- app/config/environment.js
+++ app/config/environment.js
@@ -2,7 +2,10 @@ var _ = require('lodash');
 
 var localEnvVars = {
   TITLE:      'panda_working/',
-  SAFE_TITLE: 'panda_working/'
+  SAFE_TITLE: 'panda_working/',
+  COOKIE_SECRET: 'securetocookiedesu',
+  SESSION_SECRET: 'securetoseshonudesu',
+  TOKEN_SECRET:   'superusecuretotokenodesu'
 };
 
 // Merge all environmental variables into one object.
